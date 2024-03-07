96a67bdf070858650a5a87e76af2907456e0534e
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-03-07 17:48:49 +0100

    Add header component

--- Frontend/src/components/signIn/index.tsx
+++ Frontend/src/components/signIn/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "./style.css";
 
 const SignIn = () => {
-  return (
+  const navigate = useNavigate();
+  const user = false;
+  const userName = "imene"
+  //finction for SignIn
+  function Singin() {
+    navigate("/login");
+  }
+  //finction for Singout
+  function Singout() {
+    navigate("/");
+  }
+
+  return !user ? (
     <div>
- 
+      <button onClick={Singin} className="main-nav-item">
+        <i className="fa fa-user-circle"></i>
+        Sign In
+      </button>
+    </div>
+  ) : (
+    <div className="logout">
+      <Link className="nameUser" to="/profile">
+        <i className="fa fa-user-circle"></i>
+        <p className="user name">{userName}</p>
+      </Link>
+      <button onClick={Singout} className="main-nav-item">
+        <i className="fa fa-sign-out"></i>
+        Sign out
+      </button>
     </div>
   );
 };
--- Frontend/src/containers/header/index.tsx
+++ Frontend/src/containers/header/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/argentBankLogo.png";
+import SignIn from "../../components/signIn"
 import "./style.css";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <nav className="main-nav">
+      <Link className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        ></img>
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <SignIn/>
+    </nav>
+  );
 };
 
 export default Header;
