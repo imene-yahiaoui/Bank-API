defd02b3a68ceb0243ad27dc2451d4c84159bc89
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-03-22 01:32:09 +0100

    Add home test E2E

---
+++ Frontend/cypress/E2E/pages/home.cy.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-undef */
+import { Logo, NaveBare } from "../variabeles/homeVariables.cy";
+context("Navigation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+
+  it("logo should be visible", () => {
+    cy.get(Logo).should("be.visible");
+  });
+
+  it("logo click", () => {
+    cy.get(Logo).click();
+    cy.url().should("include", "/");
+  });
+
+  it("logo should be visible", () => {
+    cy.get(NaveBare).should("be.visible");
+  });
+});
+
+// it("logo click", () => {
+//     cy.get(Logo).click();
+// cy.url().should("include", "/");
+// });
+
+//     it("logo click", () => {
+//       cy.get(".logo").click();
+//       cy.url().should("include", "/");
+//     });
+//     /**
+//      * navbar should be visible
+//      */
+//     it("navbar should be visible", () => {
+//       cy.get(".nav").should("be.visible");
+//       cy.get(".nav > :nth-child(1) > a").should("be.visible");
+//       cy.get('[href="/profil/18"] > .userBtn').should("be.visible");
+//       cy.get('[href="/profil/18"] > .userBtn').should("be.visible");
+//       cy.get(".nav > :nth-child(4) > a").should("be.visible");
+//     });
+//     /**
+//      * navbar btn click
+//      */
+//     it("navbar click", () => {
+//       cy.get(".nav > :nth-child(1) > a").click();
+//       cy.url().should("include", "/");
+//       cy.get('[href="/profil/18"] > .userBtn').click();
+//       cy.url().should("include", "/profil");
+//       cy.get(".nav > :nth-child(3) > a").click();
+//       cy.url().should("include", "/settings");
+//       cy.get(".nav > :nth-child(4) > a").click();
+//       cy.url().should("include", "/community");
+//     });
+//     /**
+//      * navbar size and dimensions
+//      */
+//     it("set the navbar size and dimensions", () => {
+//       cy.viewport(320, 480);
+//       // The navbar should collapse since our screen is smaller
+//       cy.get(".nav").should("not.be.visible");
+//     });
+//     /**
+//      * btn user should be visible
+//      */
+//     it("buttons should be visible", () => {
+//       cy.get('[href="/profil/12"] > .userBtn').should("be.visible");
+//       cy.get('[href="/profil/18"] > .userBtn').should("be.visible");
+//       cy.get('[href="/profil/1"] > .userBtn').should("be.visible");
+//       cy.get('[href="/profil/2"] > .userBtn').should("be.visible");
+//     });
+//     /**
+//      * home page  visible in dimensions 1024, 780
+//      */
+//     it("the home page should be visible in dimensions 1024, 780", () => {
+//       cy.viewport(1024, 780);
+//       cy.get(".App").should("be.visible");
+//     });
+
+//     /**
+//      * home page h1 visible
+//      */
+//     it("in home page H1 should be visible  ", () => {
+//       cy.get("h1").should("be.visible");
+//     });
---
+++ Frontend/cypress/E2E/variabeles/homeVariables.cy.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+
+export const Logo = ('.main-nav-logo-image');
+
+export const NaveBare= ('.main-nav')
